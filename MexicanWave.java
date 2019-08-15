4855bf9d98c004717b663285c28d8ee3f8f1f581
tcpackfrequency <nezpower@abv.bg>
2019-08-16 01:23:38 +0300

    Initial commit :P

---
+++ MexicanWave.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MexicanWave {
+
+    public static String[] wave(String str) {
+          List<String> wave = new ArrayList<>();
+        for(int i = 0; i < str.length(); i++){
+          StringBuilder sb = new StringBuilder();
+          char c = Character.toUpperCase(str.charAt(i));
+          if (c == ' ') continue;
+          sb.append(str);
+          sb.setCharAt(i, c);
+          wave.add(sb.toString());
+        }
+        return wave.stream().toArray(String[]::new);
+    }
+    
+}
