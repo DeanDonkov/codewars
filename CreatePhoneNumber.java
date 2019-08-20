b6551e2f2e45772422318bb461e2a90f51017a9c
TcpAckFrequency <nezpower@abv.bg>
2019-08-20 18:54:55 +0300

    Add PhoneNumber[6kyu]

---
+++ CreatePhoneNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    StringBuilder sb = new StringBuilder();
+    if(numbers.length == 10){
+      sb.append("(");
+      for(int i = 0; i <= 2; i++){
+        sb.append(numbers[i]);
+      }
+      sb.append(") ");
+      for(int i = 3; i <= 5; i++){
+        sb.append(numbers[i]);
+      }
+      sb.append("-");
+      for(int i = 6; i <= numbers.length - 1; i++){
+      sb.append(numbers[i]);
+      }
+    }
+    return sb.toString();
+  }
+}
