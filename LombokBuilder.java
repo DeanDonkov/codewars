0592a81094c26b238c615d064781ce967c759930
TcpAckFrequency <nezpower@abv.bg>
2019-08-20 18:57:32 +0300

    Add Lombok-Builder

---
+++ LombokBuilder.java
@@ -0,0 +1,26 @@
+public class People{
+ private int age;
+ private String name;
+ private String lastName;
+ private String city;
+ private String job;
+ private String GREET="hello";
+ 
+ public int getAge() { return age; }
+ public String getName() { return name; }
+ public String getLastName() { return lastName; }
+ public String getCity(){ return city;}
+ public String getJob() {return job;}
+ 
+ public String greet() { return GREET + " my name is " + name;}
+ 
+ public static People builder() { return new People(); }
+ public People name(String name){  this.name = name; return this;}
+ public People lastName(String lastname) { this.lastName = lastname; return this;}
+ public People age(int age) { this.age = age; return this; }
+ public People city(String city) { this.city = city; return this;}
+ public People job(String job){ this.job = job; return this;}
+ 
+ public People build(){ return this; }
+
+}
