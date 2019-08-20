6c82e54320b10c7b43bf183ff466d5028c289d28
TcpAckFrequency <nezpower@abv.bg>
2019-08-20 18:41:24 +0300

    Add Psychic kata

---
+++ Psychic.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.lang.reflect.*;
+public class Psychic {
+  private static Random rand;
+  
+  static{
+    rand = new Random(1);
+    for (Class c : Math.class.getDeclaredClasses()){      
+      for (Field f : c.getDeclaredFields()){
+        try{
+          f.setAccessible(true);
+          Random r = (Random)f.get(null);
+          r.setSeed(1);
+        } catch(Exception e){}
+      }
+    }
+  }
+  
+  public static double guess() {
+    return rand.nextDouble(); 
+  }
+}
