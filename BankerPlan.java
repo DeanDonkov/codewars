7f91e90cfa73512dcac4e59a1d976c11d911b451
TcpAckFrequency <nezpower@abv.bg>
2019-08-21 16:40:53 +0300

    Add BankerPlan [6kyu]

---
+++ BankerPlan.java
@@ -0,0 +1,13 @@
+public class BankerPlan {
+    
+    public static boolean fortune(int f0, double p, int c0, int n, double i) {
+        int prev_x = f0; int prev_c = c0;
+        int nou_x = -1; int nou_c = -1;
+        for (int k = 1; k < n; k++) {
+            nou_x = (int)Math.floor(prev_x + p/100.0 * prev_x - prev_c);
+            nou_c = (int)Math.floor(prev_c + i/100.0 * prev_c);
+            prev_x = nou_x; prev_c = nou_c;
+        }
+        return (nou_x >= 0);
+    }
+}
