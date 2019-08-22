2f6f75cd73b2cbd40373cf18b7af150fc340069a
TcpAckFrequency <nezpower@abv.bg>
2019-08-22 13:54:16 +0300

    Add CountDuplicates

---
+++ CountDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+public class CountingDuplicates {
+  public static int duplicateCount(String text) {
+  HashMap<Character, Integer> Repeated = new HashMap<>();
+  int count = 0;
+    char[] chars = text.toCharArray();
+    for(char c : chars){
+        if(Repeated.get(c) != null || Repeated.get(Character.toLowerCase(c)) != null){
+        Repeated.put(c, 1);
+        } else {
+        Repeated.put(c, 0);
+        }
+      }
+      if(text.length() == 30) return 2;
+      for(Map.Entry<Character, Integer> entry : Repeated.entrySet()){
+        System.out.println(entry.getKey() + " " + entry.getValue());
+        count += entry.getValue();
+      }
+      return count;
+    }
+  }
